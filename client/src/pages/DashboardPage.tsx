f93e83040de1304239cbf3b66731d8c67f4ad4fe
PedjaWasTaken <pedjamaslaric@gmail.com>
2025-12-29 05:39:27 +0100

    View of plants/fiscal account
    Added basic view, database not bound to tables
    
    *No idea if we're doing everything in English or Serbian (TODO: make language consistent)

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -6,15 +6,74 @@ type DashboardPageProps = {
     userAPI: IUserAPI;
 };
 
-export const DashboardPage : React.FC<DashboardPageProps> = ({ userAPI }) => {
-    
+export const DashboardPage: React.FC<DashboardPageProps> = ({ userAPI }) => {
     return (
-        <>
-        <div>
+        <div className="dashboard-root">
+            {/* Top navbar */}
             <DashboardNavbar userAPI={userAPI} />
+
+            {/* ZA SADA STAVIO SAM OVO OVDE, PROMENITI PAGE ILI NAEKAKO PRIKAZATI PREKO DASHBOARDA */}
+            {/* Main content */}
+            <div className="dashboard-content">
+                
+                {/* Left panel */}
+                <div className="dashboard-panel">
+                    <div className="panel-header">
+                        <span>Lista biljaka</span>
+                        <button className="panel-add">+</button>
+                    </div>
+
+                    <input
+                        className="panel-search"
+                        placeholder="Pretraga biljaka..."
+                    />
+
+                    <table className="dashboard-table">
+                        <thead>
+                            <tr>
+                                <th>Naziv</th>
+                                <th>Latinski naziv</th>
+                                <th>Jacina</th>
+                                <th>Zemlja</th>
+                                <th>Stanje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* map plants here later */}
+                        </tbody>
+                    </table>
+                </div>
+
+                {/* Right panel */}
+                <div className="dashboard-panel">
+                    <div className="panel-header">
+                        <span>Fiskalni racun</span>
+                        <button className="panel-add">+</button>
+                    </div>
+
+                    <input
+                        className="panel-search"
+                        placeholder="Pretraga racuna..."
+                    />
+
+                    <table className="dashboard-table">
+                        <thead>
+                            <tr>
+                                <th>Broj racuna</th>
+                                <th>Tip prodaje</th>
+                                <th>Nacin placanja</th>
+                                <th>Iznos (RSD)</th>
+                                <th>Datum</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* map receipts here later */}
+                        </tbody>
+                    </table>
+
+
+            </div>
         </div>
-                <h1>Dashboard</h1>
-                <p>Welcome to the dashboard!</p>
-        </>
+    </div>
     );
-}
+};
